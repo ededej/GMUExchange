9c38ac6868c6e9a7cbee60825387d1bca5152b23
ededej <ededej@masonlive.gmu.edu>
2016-10-27 10:23:39 -0400

    submission

--- app.js
+++ app.js
@@ -1,23 +1,17 @@
-var firebase = require("firebase");
 var express = require('express');
-var gcloud = require('google-cloud');
-var multer = require("multer");
-var uploader = multer({ storage: multer.memoryStorage({}) });
+var firebase = require('firebase');
 
+// var gcloud = require('google-cloud');
+// var multer = require("multer");
+// var uploader = multer({ storage: multer.memoryStorage({}) });
 var app = express();
+
 var bodyParser = require('body-parser')
 app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
     extended: true
 }));
 var port = process.env.PORT || 4000;
-
-//
-// var counter = 0;
-// app.get('/count', function (req, res) {
-//     res.send('Hello World has been said ' + counter + ' times!');
-//     counter++;
-// });
 
 //FIREBASE INTERACTION
 firebase.initializeApp({
@@ -93,74 +87,90 @@ app.post('/postItem', function (req, res) {
     });
 });
 
+//update URI to display Home page
+app.get('/home/', function(req, res){
+    res.send(req.params);
+});
+//update URI to display signUp page
+app.get('/signUp/', function(req, res){
+});
+//update URI to display logIn page
+app.get('/logIn/', function(req, res){
+});
+//update URI to display postItem page
+app.get('/postItem/', function(req, res){
+});
+//update URI to display product ID, category
+app.get('/category/:category/:id/', function(req, res){
+    res.json({"id": req.params.id},{"category":req.params.category});
+});
+//update URI to display search page
+app.get('/search/', function(req, res){
+});
+
 //GOOGLE CLOUD INTERACTION
 // The code that follows have been based on demo 15 from: https://github.com/gmu-swe432/lecture15demos/blob/master/blobstore/index.js
 
-var CLOUD_BUCKET="gmuexchange.appspot.com."; //From storage console, list of buckets https://console.cloud.google.com/storage/browser?project=gmuexchange
-var gcs = gcloud.storage({
-    projectId: '191322581126', //from storage console, then click settings, then "x-goog-project-id"
-    keyFilename: 'privkey.json' //the key we already set up
-});
-function getPublicUrl (filename) {
-    return 'https://storage.googleapis.com/' + CLOUD_BUCKET + '/' + filename;
-}
-
-var bucket = gcs.bucket(CLOUD_BUCKET);
-
-//From https://cloud.google.com/nodejs/getting-started/using-cloud-storage
-function sendUploadToGCS (req, res, next) {
-    if (!req.file) {
-        return next();
-    }
-
-    var gcsname = Date.now() + req.file.originalname;
-    var file = bucket.file(gcsname);
-
-
-    var stream = file.createWriteStream({
-        metadata: {
-            contentType: req.file.mimetype
-        }
-    });
-
-    stream.on('error', function (err) {
-        req.file.cloudStorageError = err;
-        next(err);
-    });
-
-    stream.on('finish', function () {
-        req.file.cloudStorageObject = gcsname;
-        req.file.cloudStoragePublicUrl = getPublicUrl(gcsname);
-        var options = {
-            entity: 'allUsers',
-            role: gcs.acl.READER_ROLE
-        };
-        file.acl.add(options, function(a,e){next();});//Make file world-readable; this is async so need to wait to return OK until its done
-    });
-
-    stream.end(req.file.buffer);
-}
+// var CLOUD_BUCKET="gmuexchange.appspot.com."; //From storage console, list of buckets https://console.cloud.google.com/storage/browser?project=gmuexchange
+// var gcs = gcloud.storage({
+//     projectId: '191322581126', //from storage console, then click settings, then "x-goog-project-id"
+//     keyFilename: 'privkey.json' //the key we already set up
+// });
+// function getPublicUrl (filename) {
+//     return 'https://storage.googleapis.com/' + CLOUD_BUCKET + '/' + filename;
+// }
+//
+// var bucket = gcs.bucket(CLOUD_BUCKET);
+//
+// //From https://cloud.google.com/nodejs/getting-started/using-cloud-storage
+// function sendUploadToGCS (req, res, next) {
+//     if (!req.file) {
+//         return next();
+//     }
+//
+//     var gcsname = Date.now() + req.file.originalname;
+//     var file = bucket.file(gcsname);
+//
+//
+//     var stream = file.createWriteStream({
+//         metadata: {
+//             contentType: req.file.mimetype
+//         }
+//     });
+//
+//     stream.on('error', function (err) {
+//         req.file.cloudStorageError = err;
+//         next(err);
+//     });
+//
+//     stream.on('finish', function () {
+//         req.file.cloudStorageObject = gcsname;
+//         req.file.cloudStoragePublicUrl = getPublicUrl(gcsname);
+//         var options = {
+//             entity: 'allUsers',
+//             role: gcs.acl.READER_ROLE
+//         };
+//         file.acl.add(options, function(a,e){next();});//Make file world-readable; this is async so need to wait to return OK until its done
+//     });
+//
+//     stream.end(req.file.buffer);
+// }
 //Make a new one
-app.post('/upload', uploader.single("img"), sendUploadToGCS, function (req, res, next) {
-    console.log("Uplaoding");
-    var data = req.body.formData;
-    // console.log(data);
+// app.get('/upload', uploader.single("img"), sendUploadToGCS, function (req, res, next) {
+//     console.log("Uplaoding");
+//     var data =  req.file.cloudStorageObject;
+//     console.log("In upload : " + data);
+//     if(req.file)
+//         console.log("adding new image");
+//         data.img = getPublicUrl(req.file.cloudStorageObject);
+//     firePostProduct.push(data, function () {
+//             res.send("OK!");
+//     }).catch(function(){
+//         res.status(403);
+//         res.send();
+//     });
+// });
 
-    if(req.file)
-        console.log("adding new image");
-        data.img = getPublicUrl(req.file.cloudStorageObject);
-    firePostProduct.push(data, function () {
-            res.send("OK!");
-    }).catch(function(){
-        res.status(403);
-        res.send();
-    });
-});
-
-app.get('/emptyHtml.html', function (req, res) {
-    console.log("Requested empty html");
-    res.send("OK!");
-});
 
 app.use(express.static('public')); // has to be last thing
 
